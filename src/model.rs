adca8c62647c802e14f18e5e40e7ab4131a9cf15
Ivan <ivan390@abv.bg>
2024-01-28 18:51:47 +0200

    added board piece positioning

--- src/model.rs
+++ src/model.rs
@@ -1,25 +1,33 @@
 mod pieces;
 
-enum Color {
+pub enum Color {
     Red,
     Green,
     Yellow,
     Blue,
 }
 
-enum Square {
+pub enum Square {
     None,
     Normal(Color),
     Ghost(Color),
 }
 
-trait Piece {
-    fn get_position(&self) -> (usize, usize);
-    fn set_position(&mut self, position: (usize, usize));
+pub trait Piece {
+    fn get_position(&self) -> (i32, i32);
+    fn set_position(&mut self, position: (i32, i32));
     fn get_color(&self) -> Color;
-    fn get_squares(&self) -> Vec<(usize, usize)>;
+    fn get_squares(&self) -> Vec<(i32, i32)>;
     fn rotate_left(&mut self) -> bool;
     fn rotate_right(&mut self) -> bool;
+}
+
+
+pub trait BoardListener {
+    fn on_line_cleared(&self, lines_y: Vec<usize>);
+    fn on_game_over(&self);
+    fn on_piece_set(&self);
+    fn request_new_pice(&self) -> Box<dyn Piece>; 
 }
 
 struct Board {
@@ -27,5 +35,159 @@ struct Board {
     width: usize,
     height: usize,
     active_piece: Box<dyn Piece>,
+    listener: Box<dyn BoardListener>,
+}
+impl Board {
+    fn new(width: usize, height: usize, starting_piece: Box<dyn Piece>, board_listener: Box<dyn BoardListener>) -> Result<Board, String> {
+        if width < 4 || height < 6 {
+            return Err("Invalid width or height".to_string());
+        }
 
+        let mut board = Board{width: width, height: height, squares: vec![], active_piece: starting_piece, listener: board_listener};
+
+        Ok(board)
+    }
+
+    fn get_active_piece_squares(&self) -> (Vec<(i32, i32)>, Color) {
+        (self.active_piece.get_squares(), self.active_piece.get_color())
+    }
+
+    fn get_width(&self) -> usize {
+        self.width
+    }
+
+    fn get_height(&self) -> usize {
+        self.height
+    }
+    
+    fn get_square(&self, position: (usize, usize)) -> Option<&Square> {
+        if position.0 >= self.width || position.1 >= self.height {
+            None
+        }else{
+            Some(&self.squares[position.0 + self.width*position.1])
+        }
+    }
+
+    fn move_active_left(&mut self) -> bool {
+        assert!(self.is_active_piece_position_valid(&self.active_piece.get_squares()));
+        let pos = self.active_piece.get_position();
+        self.active_piece.set_position((pos.0-1, pos.1));
+        
+        if self.is_active_piece_position_valid(&self.active_piece.get_squares()) {
+            true
+        }else{
+            self.active_piece.set_position(pos);
+            false
+        }
+    }  
+
+    fn move_active_right(&mut self) -> bool {
+        assert!(self.is_active_piece_position_valid(&self.active_piece.get_squares()));
+        let pos = self.active_piece.get_position();
+        self.active_piece.set_position((pos.0+1, pos.1));
+        
+        if self.is_active_piece_position_valid(&self.active_piece.get_squares()) {
+            true
+        }else{
+            self.active_piece.set_position(pos);
+            false
+        }
+    }  
+
+    fn move_active_down(&mut self) -> bool {
+        assert!(self.is_active_piece_position_valid(&self.active_piece.get_squares()));
+        let pos = self.active_piece.get_position();
+        self.active_piece.set_position((pos.0, pos.1+1));
+        
+        if self.is_active_piece_position_valid(&self.active_piece.get_squares()) {
+            true
+        }else{
+            self.active_piece.set_position(pos);
+            false
+        }
+    }
+
+    fn rotate_active_left(&mut self) -> bool {
+        assert!(self.is_active_piece_position_valid(&self.active_piece.get_squares()));
+        if !self.active_piece.rotate_left() {
+            return false;
+        }
+
+        if self.is_active_piece_position_valid(&self.active_piece.get_squares()) {
+            true
+        }else{
+            self.active_piece.rotate_right();
+            false
+        }
+    }
+
+    fn rotate_active_right(&mut self) -> bool {
+        assert!(self.is_active_piece_position_valid(&self.active_piece.get_squares()));
+        if !self.active_piece.rotate_right() {
+            return false;
+        }
+
+        if self.is_active_piece_position_valid(&self.active_piece.get_squares()) {
+            true
+        }else{
+            self.active_piece.rotate_left();
+            false
+        }
+    }
+
+    fn is_active_piece_position_valid(&self, active_squares: &Vec<(i32, i32)>) -> bool {
+        for i in active_squares {
+            if i.0 < 0 || i.0 >= self.width as i32 || i.1 < 0 || i.1 >= self.height as i32 {
+                return false;
+            }
+
+            let square = self.get_square((i.0 as usize, i.1 as usize)).unwrap();
+            match square {
+                Square::Normal(_) => {
+                    return false;
+                },
+                _ => {
+                    continue;
+                }
+            }
+
+        }
+
+        true
+    }
+
+    fn remove_ghost_pieces(&mut self) {
+        for i in 0..self.squares.len() {
+            match self.squares[i] {
+                Square::Ghost(_) => { self.squares[i] = Square::None}
+                _ => {continue;},
+            }
+        }
+    }
+
+    fn position_ghost_pieces(&mut self) {
+        self.remove_ghost_pieces();
+
+        let mut active_squares = self.active_piece.get_squares();
+
+        while self.is_active_piece_position_valid(&active_squares) {
+            active_squares = active_squares
+                .iter()
+                .map(|x| {(x.0, x.1+1)})
+                .collect();
+        }
+
+        active_squares = active_squares
+                .iter()
+                .map(|x| {(x.0, x.1-1)})
+                .collect();
+
+        for i in active_squares {
+            let pos: usize = (i.0 + i.1*self.width as i32) as usize;
+            match self.squares[pos] {
+                Square::None => { self.squares[pos] = Square::Ghost(self.active_piece.get_color())},
+                _ => {panic!("Error positioning ghost pieces")},
+            }
+        }
+    }
 }
--- src/model/pieces.rs
+++ src/model/pieces.rs
@@ -2,16 +2,16 @@ use crate::model::Piece;
 
 use super::Color;
 
-struct SquarePiece {
-    position: (usize, usize),
-    squares: Vec<(usize, usize)>,
+pub struct SquarePiece {
+    position: (i32, i32),
+    squares: Vec<(i32, i32)>,
 }
 impl Piece for SquarePiece {
-    fn get_position(&self) -> (usize, usize) {
+    fn get_position(&self) -> (i32, i32) {
         self.position
     }
 
-    fn set_position(&mut self, position: (usize, usize)) {
+    fn set_position(&mut self, position: (i32, i32)) {
         self.position.0 = position.0;
         self.position.1 = position.1;
         
@@ -21,7 +21,7 @@ impl Piece for SquarePiece {
         Color::Yellow
     }
 
-    fn get_squares(&self) -> Vec<(usize, usize)> {
+    fn get_squares(&self) -> Vec<(i32, i32)> {
         self.squares
             .iter()
             .map(|x| {(x.0+self.position.0, x.1+self.position.1)})
@@ -36,6 +36,18 @@ impl Piece for SquarePiece {
         false
     }
 }
+impl SquarePiece {
+    pub fn new(position: (i32, i32)) -> SquarePiece {
+        let mut square_piece = SquarePiece{position: position, squares: vec![]};
+        square_piece.squares.push((0,0));
+        square_piece.squares.push((1,0));
+        square_piece.squares.push((0,1));
+        square_piece.squares.push((1,1));
+
+        square_piece
+    }
+}
+
 
 enum Rotation {
     Up,
@@ -43,18 +55,38 @@ enum Rotation {
     Down,
     Left,
 }
+impl Rotation {
+    fn rotate_right(&mut self) {
+        match self {
+            Rotation::Up => {*self = Rotation::Right},
+            Rotation::Right => {*self = Rotation::Down},
+            Rotation::Down => {*self = Rotation::Left},
+            Rotation::Left => {*self = Rotation::Up},
+        }
+    }
 
-struct LPiece {
-    position: (usize, usize),
-    squares: Vec<Vec<(usize, usize)>>,
+    fn rotate_left(&mut self) {
+        match self {
+            Rotation::Up => {*self = Rotation::Left},
+            Rotation::Right => {*self = Rotation::Up},
+            Rotation::Down => {*self = Rotation::Right},
+            Rotation::Left => {*self = Rotation::Down},
+        }
+    }
+}
+
+
+pub struct LPiece {
+    position: (i32, i32),
+    squares: Vec<Vec<(i32, i32)>>,
     rotation: Rotation
 }
 impl Piece for LPiece {
-    fn get_position(&self) -> (usize, usize) {
+    fn get_position(&self) -> (i32, i32) {
         self.position
     }
 
-    fn set_position(&mut self, position: (usize, usize)) {
+    fn set_position(&mut self, position: (i32, i32)) {
         self.position.0 = position.0;
         self.position.1 = position.1;
         
@@ -64,7 +96,7 @@ impl Piece for LPiece {
         Color::Yellow
     }
 
-    fn get_squares(&self) -> Vec<(usize, usize)> {
+    fn get_squares(&self) -> Vec<(i32, i32)> {
         match self.rotation {
             Rotation::Up => {
                 &self.squares[0]
@@ -84,13 +116,45 @@ impl Piece for LPiece {
             .collect()
     }
     
-    fn rotate_left(&mut self) -> bool {
-        todo!();
+    fn rotate_right(&mut self) -> bool {
+        self.rotation.rotate_right();
+        
         true
     }
 
-    fn rotate_right(&mut self) -> bool {
-        todo!();
+    fn rotate_left(&mut self) -> bool {
+        self.rotation.rotate_left();
+
         true
+    }
+}
+impl LPiece {
+    pub fn new(position: (i32, i32)) -> LPiece {
+        let mut l_piece = LPiece{position: position, squares: vec![], rotation: Rotation::Up};
+        l_piece.squares.push(vec![]);
+        l_piece.squares[0].push((0,0));
+        l_piece.squares[0].push((1,0));
+        l_piece.squares[0].push((1,1));
+        l_piece.squares[0].push((1,2));
+
+        l_piece.squares.push(vec![]);
+        l_piece.squares[1].push((0,0));
+        l_piece.squares[1].push((0,1));
+        l_piece.squares[1].push((-1,1));
+        l_piece.squares[1].push((-2,1));
+
+        l_piece.squares.push(vec![]);
+        l_piece.squares[2].push((0,0));
+        l_piece.squares[2].push((0,1));
+        l_piece.squares[2].push((0,2));
+        l_piece.squares[2].push((1,2));
+
+        l_piece.squares.push(vec![]);
+        l_piece.squares[3].push((0,0));
+        l_piece.squares[3].push((0,1));
+        l_piece.squares[3].push((0,2));
+        l_piece.squares[3].push((0,1));
+
+        l_piece
     }
 }
